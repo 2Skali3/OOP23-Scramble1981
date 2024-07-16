fccacdbd6fc834fcfd34a7233fc2b63553dff2dc
2Skali3 <130842781+2Skali3@users.noreply.github.com>
2024-07-16 11:36:24 +0200

    created the class Point2D and implemented it

--- app/src/main/java/scramble1981_remastered/model/common/api/Point2D.java
+++ app/src/main/java/scramble1981_remastered/model/common/api/Point2D.java
@@ -1,9 +1,9 @@
 package scramble1981_remastered.model.common.api;
 
-public interface Position2D {
-
+public interface Point2D {
     int getX();
 
     int getY();
 
+    void setLocation(int x, int y);
 }
