185365695131f25fd2c8000fe99ffba13289b61a
Francesco Teo Calzolari <Francesco.calzolari7@studio.unibo.it>
2024-08-20 10:29:47 +0200

    delete updateSprite from RocketImpl

--- app/src/main/java/scramble/model/enemy/impl/RocketImpl.java
+++ app/src/main/java/scramble/model/enemy/impl/RocketImpl.java
@@ -10,7 +10,7 @@ import scramble.model.enemy.api.Enemy;
  * Implementation of the Enemy interface. Used for the simple rocket NPC.
  */
 public class RocketImpl extends GameElementImpl implements Enemy {
-    
+
     /**
      * Class constructor.
      *
@@ -18,34 +18,33 @@ public class RocketImpl extends GameElementImpl implements Enemy {
      * @param y      Y coordinate
      * @param width  Rocket width
      * @param height Rocket height
-    */
-    public RocketImpl(final int x, final int y, final int width, final int height){
+     */
+    public RocketImpl(final int x, final int y, final int width, final int height) {
         super(x, y, width, height);
     }
+
     /** {@inheritDoc} */
     @Override
     public void startMovement() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'startMovment'");
     }
+
     @Override
     public void updatePosition(PairImpl<Integer, Integer> newPosition) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updatePosition'");
     }
+
     @Override
     public PairImpl<Integer, Integer> getPosition() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
     }
+
     @Override
     public BufferedImage getSprite() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getSprite'");
-    }
-    @Override
-    public void updateSprite(BufferedImage newSprite) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateSprite'");
     }
 }
